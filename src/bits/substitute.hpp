2b77dd7e6aff0d0ffb78436f36e85990e320079f
koftamainee <koftamainee@gmail.com>
2026-02-13 12:25:31 +0300

    substitute

--- src/bits/substitute.hpp
+++ src/bits/substitute.hpp
@@ -1,18 +1,27 @@
-#ifdef CRYPTO_BITS_SUBSTITUTE_HPP
+#ifndef CRYPTO_BITS_SUBSTITUTE_HPP
 #define CRYPTO_BITS_SUBSTITUTE_HPP
 
+#include <array>
+#include <cstdint>
 #include <functional>
 #include <unordered_map>
 #include <vector>
 
-namespace crypcrypto::bits {
+namespace crypto::bits {
 
 std::vector<uint8_t> substitute(const std::vector<uint8_t> &bits,
-                                const std::unordered_map &s_block);
+                                const std::array<uint8_t, 256> &s_block,
+                                size_t block_size_in, size_t block_size_out);
+
+std::vector<uint8_t>
+substitute(const std::vector<uint8_t> &bits,
+           const std::unordered_map<uint8_t, uint8_t> &s_block,
+           size_t block_size_in, size_t block_size_out);
 
 std::vector<uint8_t> substitute(const std::vector<uint8_t> &bits,
-                                const std::function s_block);
+                                const std::function<uint8_t(uint8_t)> &s_block,
+                                size_t block_size_in, size_t block_size_out);
 
-} // namespace crypcrypto::bits
+} // namespace crypto::bits
 
 #endif // CRYPTO_BITS_SUBSTITUTE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include "../tests/common.hpp"
+#include <array>
+#include <cstdint>
 #include <iostream>
+#include <vector>
+
+#include "bits/substitute.hpp"
+
+using namespace crypto::bits;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::vector<uint8_t> bits = {0b00001111};
+  std::array<uint8_t, 256> s_block;
+
+  for (int i = 0; i < 16; i++)
+    s_block[i] = i;
+
+  auto out = substitute(bits, s_block, 4, 2);
+  printf("%b\n", out[0]);
 }
